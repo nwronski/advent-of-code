ac811616014b11886339e697d9ad7c3bbe5551e1
Nick Wronski <nwronski@gmail.com>
2020-12-02 14:58:18 -0500

    initial repo setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { readFile } from 'fs';
+import { resolve } from 'path';
+import { promisify } from 'util';
+
+const readFileAsync = promisify(readFile);
+
+export type SolutionFunc = (contents: string) => Record<string, unknown>;
+
+async function run(day: string) {
+  const { solution } = await import(`./day-${day}.ts`) as { solution: SolutionFunc; };
+  const contents = await readFileAsync(resolve(`./input/day-${day}.txt`), 'utf-8');
+  return solution(contents.trim());
+}
+
+// eslint-disable-next-line no-console
+run(process.argv[2]).then(console.log, console.error);
